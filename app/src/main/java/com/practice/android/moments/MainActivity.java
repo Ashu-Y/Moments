8bcafff755955ed1af1dc935f810cbd43571f32d
Hitesh Goel <hitesh.goel.15csc@bml.edu.in>
2017-06-13 16:56:11 +0530

    Login java and signup activity

--- app/src/main/java/com/practice/android/moments/MainActivity.java
+++ app/src/main/java/com/practice/android/moments/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class MainActivity extends AppCompatActivity {
+
+    //variables
     EditText login, pass; // login and password edittext
     Button signin, signup; // sign in  and sign up button
     GoogleSignInOptions gso;//google sign button
@@ -17,5 +19,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+// giving data from xml to variables
+        login = (EditText) findViewById(R.id.Emailogin);
+        pass = (EditText) findViewById(R.id.passlogin);
+
+        signin = (Button) findViewById(R.id.button);
+        signup = (Button) findViewById(R.id.button2);
+
+
+
+
+
+
+
+
     }
 }
