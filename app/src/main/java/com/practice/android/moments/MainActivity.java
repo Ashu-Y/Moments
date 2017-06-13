236ab6f84d3594c5b93bc8a88df39751f0220baa
Hitesh Goel <hitesh.goel.15csc@bml.edu.in>
2017-06-13 16:50:07 +0530

    Login java

--- app/src/main/java/com/practice/android/moments/MainActivity.java
+++ app/src/main/java/com/practice/android/moments/MainActivity.java
@@ -1,9 +1,17 @@
 package com.practice.android.moments;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class MainActivity extends AppCompatActivity {
+    EditText login, pass; // login and password edittext
+    Button signin, signup; // sign in  and sign up button
+    GoogleSignInOptions gso;//google sign button
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
