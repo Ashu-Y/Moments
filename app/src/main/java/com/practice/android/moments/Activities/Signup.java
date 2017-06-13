c72337c352331d2f5cdfe3ce62e5300ee27755c4
Ashutosh <ashutoshyadav102@gmail.com>
2017-06-13 17:24:44 +0530

    Created Timeline Activity

--- app/src/main/java/com/practice/android/moments/Activities/Signup.java
+++ app/src/main/java/com/practice/android/moments/Activities/Signup.java
@@ -2,6 +2,7 @@ package com.practice.android.moments.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.practice.android.moments.R;
 
@@ -11,5 +12,7 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Toast.makeText(Signup.this, "sfdsgs", Toast.LENGTH_SHORT).show();
     }
 }
