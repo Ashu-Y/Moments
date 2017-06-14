e0cdc74d5715c511cceb1be26a2cfdde2db51c6a
Hitesh Goel <hitesh.goel.15csc@bml.edu.in>
2017-06-14 11:17:11 +0530

    firebase and permission

--- app/src/main/java/com/practice/android/moments/Activities/MainActivity.java
+++ app/src/main/java/com/practice/android/moments/Activities/MainActivity.java
@@ -76,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, Signup.class));
+                finish();
             }
         });
 
