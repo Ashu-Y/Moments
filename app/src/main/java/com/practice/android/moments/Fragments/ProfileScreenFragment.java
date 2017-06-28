8397b28b02cef354b14ddad3279d69ba4ac27fa7
Hitesh Goel <hitesh.goel.15csc@bml.edu.in>
2017-06-28 16:58:57 +0530

    update photo

--- app/src/main/java/com/practice/android/moments/Fragments/ProfileScreenFragment.java
+++ app/src/main/java/com/practice/android/moments/Fragments/ProfileScreenFragment.java
@@ -20,14 +20,10 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.practice.android.moments.Profile_model_class;
 import com.practice.android.moments.R;
 
 import java.io.File;
@@ -218,26 +214,27 @@ public class ProfileScreenFragment extends Fragment {
 
         String user_id = firebaseuser.getUid();
 
-        databaseReference.child(user_id).child("User Info").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Profile_model_class user = dataSnapshot.getValue(Profile_model_class.class);
+//        databaseReference.child(user_id).child("User Info").addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                Profile_model_class user = dataSnapshot.getValue(Profile_model_class.class);
+//
+//                assert user != null;
+//                Log.d(TAG, "User name: " + user.getName() + ", email " + user.getEmail());
+//
+//
+//                String pic = user.getPhoto();
+//                Uri pic_uri = Uri.parse(pic);
+//                Picasso.with(getActivity()).load(pic_uri).fit().centerCrop().into(profile_pic);
+        Picasso.with(getActivity()).load(download_uri).fit().centerCrop().into(profile_pic);
 
-                assert user != null;
-                Log.d(TAG, "User name: " + user.getName() + ", email " + user.getEmail());
+//            }
 
-
-                String pic = user.getPhoto();
-                Uri pic_uri = Uri.parse(pic);
-                Picasso.with(getActivity()).load(pic_uri).fit().centerCrop().into(profile_pic);
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
 
 
     }
