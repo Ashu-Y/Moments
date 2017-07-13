affdf6028df3df3b931f10c725c8485fc7e4983d
Ashutosh <ashutoshyadav102@gmail.com>
2017-07-13 14:50:36 +0530

    Fixed bugs

--- app/src/main/java/com/practice/android/moments/Fragments/ProfileFragment.java
+++ app/src/main/java/com/practice/android/moments/Fragments/ProfileFragment.java
@@ -17,13 +17,18 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.practice.android.moments.Helper.ProfileViewHelper;
 import com.practice.android.moments.Models.Image;
+import com.practice.android.moments.Models.Profile_model_class;
 import com.practice.android.moments.R;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -63,12 +68,9 @@ public class ProfileFragment extends Fragment {
         context = getContext();
 
 
-
         RecyclerView.LayoutManager lm_recycle = new GridLayoutManager(getContext(), 2);
         recyclerView.getRecycledViewPool().clear();
         recyclerView.setLayoutManager(lm_recycle);
-
-
 
 
         settings.setOnClickListener(new OnClickListener() {
@@ -125,7 +127,33 @@ public class ProfileFragment extends Fragment {
 
 
         databaseReference = FirebaseDatabase.getInstance().getReference()
-                .child("Users").child(user_id).child("User Pictures");
+                .child("Users").child(user_id);
+
+
+
+        databaseReference.child("User Info").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Profile_model_class user = dataSnapshot.getValue(Profile_model_class.class);
+
+                assert user != null;
+                Log.d(TAG, "User name: " + user.getName() + ", email " + user.getEmail() + "    " + user.getRelationship() + "    " + user.getAbout());
+                profilename.setText(user.getName());
+
+                Glide.with(getContext()).load(user.getPhoto()).placeholder(R.drawable.c1).into(profile);
+                Glide.with(getContext()).load(user.getCoverPhoto()).placeholder(R.drawable.c1).into(coverpic);
+
+                Log.d(TAG, "\n" + user.getPhoto() + "        " + user.getGender() + "    " + user.getPhoto() + "    " + user.getCoverPhoto());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
 
 
         FirebaseRecyclerAdapter<Image, ProfileViewHelper> firebaseRecyclerAdapter = new
@@ -134,7 +162,7 @@ public class ProfileFragment extends Fragment {
                         Image.class,
                         R.layout.res_thumbnail,
                         ProfileViewHelper.class,
-                        databaseReference.orderByPriority()
+                        databaseReference.child("User Pictures").orderByPriority()
 
 
                 ) {
@@ -154,6 +182,8 @@ public class ProfileFragment extends Fragment {
 
         firebaseRecyclerAdapter.notifyDataSetChanged();
         recyclerView.setAdapter(firebaseRecyclerAdapter);
+
+
 
     }
 
